8e3be6eb32eadac75a90b04fcb16b043d04cd11e
mmarkus13 <mmarkus13@users.noreply.github.com>
2020-06-11 11:50:22 +0200

    Add files via upload

---
+++ google-search-clipboard/gsc.py
@@ -0,0 +1,16 @@
+import pyperclip
+import webbrowser
+
+s = pyperclip.paste()
+brave_path = 'C:/Program Files (x86)/BraveSoftware/Brave-Browser/Application/brave.exe -incognito %s'
+#chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe -incognito %s'
+#firefox_path = 'C:/Program Files/Mozilla Firefox/firefox.exe -private %s'
+url = ("https://www.google.com/search?q=" + s)
+webbrowser.get(brave_path).open(url)
+#webbrowser.get(chrome_path).open(url)
+#webbrowser.get(firefox_path).open(url)
+
+"""Chrome & Brave-WebBrowser opens new private window even when there is already a normal chrome window opened; 
+firefox doesn't.
+If you want to use firefox then uncomment line 7 & 11; and put "#" in at the beginning of the 
+lines above (line 4 and line 8) """
---
+++ google-search-clipboard/readme.txt
@@ -0,0 +1,5 @@
+This script is hihgly effective combined with http://capture2text.sourceforge.net/ to quickly 'google search' any text from places you can't highlight (text from image #ocr)
+
+Keybinds:
+Cap2Text captures area with Win+Q by default.
+You can create a shortcut (must be placed to Desktop) for the gsc.py script and bind it to ctrl+alt+R for example. (Note: avoid any keys that interact with AltGr - because it will overwrite functionality; for example "ctrl+alt+w" = "AltGr+W" which means that you will no longer be able to type pipe "|" with AltGr+W )
